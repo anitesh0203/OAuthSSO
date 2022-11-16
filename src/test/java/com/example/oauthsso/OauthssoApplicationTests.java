cd1839e43a71efaa67dfc979dd686d96f147e4ca
anitesh0203 <aniteshkumar2012@gmail.com>
2022-11-16 16:26:47 -0500

    Pushing the files to implement OAuth using github sso

---
+++ src/test/java/com/example/oauthsso/OauthssoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthssoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
